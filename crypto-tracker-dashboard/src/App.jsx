3ec407018059883eb366881c84768e8700450949
Esther Toyin <amooesther2503@gmail.com>
2024-10-20 15:55:01 +0100

    file directory

--- crypto-tracker-dashboard/src/App.jsx
+++ crypto-tracker-dashboard/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import { Routes,Route } from 'react-router-dom'
-import Home from './assets/Pages/Home/Home'
-import Coin from './assets/Pages/Coin/Coin'
 import Footer from './Components/Footer/Footer'
+import Home from './Pages/Home/Home'
+import Coin from './Pages/Coin/Coin'
 
 
 
